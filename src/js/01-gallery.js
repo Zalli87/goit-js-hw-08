bd16f921bec30a9107368654c88fa56b08c9d23e
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2022-11-26 18:05:01 +0200

    додає виконання 1-го і 2-го завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryItem = createGalleryItemMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+ };
+
+
+function createGalleryItemMarkup(items) {
+    return items.map(({original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${original}" alt="${description}" />
+</a>`
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionSelector: 'img', captionPosition: 'bottom', captionsData: "alt", captionDelay: 250});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+const formData = {};
+
+getLocalStorageFormData();
+
+form.addEventListener('input', throttle(onFormIntutChange, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormIntutChange(evt) {
+    formData[evt.target.name] = evt.target.value;
+    const localStorageFormData = JSON.stringify(formData);
+    localStorage.setItem('feedback-form-state', localStorageFormData); 
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    localStorage.removeItem('feedback-form-state')
+    evt.currentTarget.reset();
+    console.log(formData);
+}
+
+function getLocalStorageFormData() {
+    const savedFormData = localStorage.getItem('feedback-form-state');
+    const parseData = JSON.parse(savedFormData);
+    if (parseData) {
+        const { email, message } = parseData;
+        formData.email = email;
+        formData.message = message;
+        input.value = parseData.email ? parseData.email : '';
+        textarea.value = parseData.message ? parseData.message : '';
+ }
+}
